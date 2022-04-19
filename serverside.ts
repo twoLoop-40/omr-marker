42a43446856221851fb214a26ef5cbb9de9ca8d5
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-19 20:56:53 +0900

    ts change

---
+++ serverside.ts
@@ -0,0 +1,25 @@
+
+const doGet = () => {
+  return HtmlService.createTemplateFromFile("index.html").evaluate();
+};
+
+const include = (filename: string) => {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+};
+
+
+function pipe(...fns: Function[]) {
+  return (y: any): any => fns.reduce((prevVal, currFn) => currFn(prevVal), y);
+}
+
+interface UserInfo {
+  userCode: string
+  userName: string
+}
+
+function nameTemplate(sep) {
+  return (userInfo: UserInfo): string => {
+    const { userCode, userName } = userInfo;
+    return `${userCode}${sep}${userName}`;
+  };
+}
