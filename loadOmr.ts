7481eccddc6c4f724327998e64afbaac19aca87a
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-21 16:17:20 +0900

    class Student

--- loadOmr.ts
+++ loadOmr.ts
@@ -1,4 +1,3 @@
-
 function fileToData64 (): (arg: string) => string {
   const loadFileFromId = (fileId: string) => {
     return DriveApp.getFileById(fileId);
@@ -29,23 +28,67 @@ function fileDataToClient (fileId: string) {
   return worker(fileId);
 };
 
-function formatDate (date: Date) {
-  return `${date.getMonth()}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-};
+type InputData = Date | string | number
+class Student {
+  public studentExamData: InputData[]
+  typeToString (item: InputData) {
+    if (typeof item == 'string') return item 
+    else return item.toString()
+  }
+  dateToString () {
+    const date: InputData = this.studentExamData[0]
+    if (typeof date == 'string') return date
+    if (typeof date == 'number') return date.toString()
+    else {
+      return `${date.getMonth()}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+    }  
+  }
+  getUserCode () {
+    return this.typeToString(this.studentExamData[2])
+  }
+  getUserName () {
+    return this.typeToString(this.studentExamData[1])
+  }
+  getExamCode () {
+    return this.typeToString(this.studentExamData[3])
+  }
+  getAnswers () {
+    const examAnswers = this.studentExamData.slice(4, 19)
+    const iter = (exams: InputData | InputData[]) => {
+      if(!Array.isArray(exams)) return [this.typeToString(exams)]
+      else { 
+        return exams.flatMap(this.getAnswers)
+      }
+    }
+    return iter(examAnswers) 
+  }
+  constructor(examData: InputData[]) {
+    this.studentExamData = examData
+  }
+}
+function getWorkingSpreadsheet () {
+  const spreadsheetIds = {
+    kiwi: '1Z_6B89U_pZCX54F0SP2AczCyiNBErl1p7Wx8k_nVXOc'
+  }
+  
+}
 
-const getAnswerSheet = () => {
-  const answerSheetId = "122Efncjwc8BKq9tUKNAdPXVwSxDluEcNxNjCH8GaZGo";
-  const ss = SpreadsheetApp.openById(answerSheetId);
-  const sheetData = ss.getRange("A2:S50").getValues();
-  let toStingData = [];
-  sheetData
-    .filter((line) => !line.slice(2, 4).includes(""))
-    .forEach((data) => {
-      toStingData.push([
-        formatDate(data[0]),
-        ...data.splice(1).map((value) => String(value)),
-      ]);
-    });
-  // console.log(reframedData);
-  return toStingData;
-};
+function getStudents (id: string) {
+  
+}
+// const getAnswerSheet = () => {
+//   const answerSheetId = "122Efncjwc8BKq9tUKNAdPXVwSxDluEcNxNjCH8GaZGo";
+//   const ss = SpreadsheetApp.openById(answerSheetId);
+//   const sheetData = ss.getRange("A2:S50").getValues();
+//   let toStingData = [];
+//   sheetData
+//     .filter((line) => !line.slice(2, 4).includes(""))
+//     .forEach((data) => {
+//       toStingData.push([
+//         formatDate(data[0]),
+//         ...data.splice(1).map((value) => String(value)),
+//       ]);
+//     });
+//   // console.log(reframedData);
+//   return toStingData;
+// };
