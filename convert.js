915a827e5208403375728c16193f5032cb2fbb88
Joon Ho Lee <jh.lee@spira-t.com>
2022-04-26 03:22:00 +0900

    html maker complete

--- convert.js
+++ convert.js
@@ -1,4 +1,36 @@
 var fs = require('fs').promises;
 var path = require('path');
-console.log(typeof fs);
-fs.readFile('./hello.txt', 'utf8').then(function (content) { return console.log(content); });
+var shell = require('shelljs');
+var fileName = process.argv[2];
+function makeJsFileSrc() {
+    var getPath = function (fileName) {
+        return path.join('.', fileName);
+    };
+    var tsName = getPath(fileName);
+    shell.exec("tsc ".concat(tsName));
+    var jsName = extChange(tsName, 'js');
+    return fs.readFile(jsName, 'utf8');
+}
+function extChange(fileName, ext) {
+    var _a = [path.dirname(fileName), path.basename(fileName, 'ts')], dirName = _a[0], baseName = _a[1];
+    return path.join(dirName, baseName + "".concat(ext));
+}
+function makeHtmlFile(jsSource, fileName) {
+    var attatchWords = function (src, item) {
+        return item[0] + '\n' + src + '\n' + item[1];
+    };
+    var sendSource = function (src, destination) {
+        var longName = extChange(path.join(destination, fileName), 'html');
+        fs.writeFile(longName, src)
+            .then(function () { return console.log('HTML made!'); })
+            .then(function () { return fs.unlink(extChange(path.join('.', fileName), 'js')); })
+            .then(function () { return console.log('js file deleted'); })["catch"](function (err) { return console.error(err); });
+    };
+    jsSource.then(function (source) { return attatchWords(source, ['<script>', '</script>']); })
+        .then(function (source) { return sendSource(source, './front'); });
+}
+function test() {
+    var jsSrc = makeJsFileSrc();
+    makeHtmlFile(jsSrc, fileName);
+}
+test();
--- convert.ts
+++ convert.ts
@@ -1,5 +1,51 @@
 const fs = require('fs').promises
 const path = require('path')
+const shell = require('shelljs')
 
-console.log(typeof fs)
-fs.readFile('./hello.txt', 'utf8').then((content: string)  => console.log(content))
+const fileName: string = process.argv[2]
+
+function makeJsFileSrc (): Promise<string> {
+	type OneArg <T> = {
+		(arg: T): T
+	}	
+	const getPath: OneArg<string> = (fileName) => {
+		return path.join('.', fileName)
+	}
+	const tsName = getPath(fileName)
+	shell.exec(`tsc ${tsName}`)
+	const jsName = extChange(tsName, 'js')
+	return fs.readFile(jsName, 'utf8')
+}
+
+function extChange (fileName: string, ext: string): string {
+	const [dirName, baseName] = [path.dirname(fileName), path.basename(fileName, 'ts')]
+	return path.join(dirName, baseName + `${ext}`)	
+}
+
+function makeHtmlFile (jsSource: Promise<string>, fileName: string) {
+	type HeadTail = [string, string]
+	const attatchWords = (src: string, item: HeadTail) => {
+		return item[0] + '\n' + src + '\n' + item[1]
+	}
+	const sendSource = (src: string, destination: string) => {
+		const longName = extChange(
+			path.join(destination, fileName),
+			'html')
+		fs.writeFile(longName, src)
+			.then(() => console.log('HTML made!'))
+			.then(() => fs.unlink(extChange(path.join('.', fileName), 'js')))
+			.then(() => console.log('js file deleted'))
+			.catch((err: Error) => console.error(err))
+	}
+
+	jsSource.then((source: string) => attatchWords(source, ['<script>', '</script>']))
+		.then((source: string) => sendSource(source, './front'))
+}
+
+function test () {
+	const jsSrc = makeJsFileSrc()	
+	makeHtmlFile(jsSrc, fileName)
+}
+test()
+
+	
